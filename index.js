4e8718f7ad116e5ac7a8dd5edfa414f412ad7322
Nuo <nuot@uci.edu>
2019-05-08 01:27:08 -0700

    add view products function

--- index.js
+++ index.js
@@ -6,10 +6,57 @@ const db = createConnection({
     port: 3306,
     user: 'root',
     password: `fghjkl;'`,
-    database:'bamazon_db'
+    database: 'bamazon_db'
 })
 
+
+async function viewProducts() {
+    let response = await new Promise((res, rej) => {
+        db.query('SELECT * FROM products', (e, r) => {
+            if (e) {
+                rej (e)
+            } else{
+                res (r)
+            } 
+           
+        })
+    })
+       return response
+}
+
 // inquirer prompt for action
+const getStart = _ => {
+    prompt({
+        type: 'list',
+        name: 'starter',
+        message: 'Welcome to bamazon! What do you like to do?',
+        choices: ['View all products.', 'Search product and price.', 'Make purchase.', '--EXIT--']
+    })
+        .then(({ starter }) => {
+            switch (starter) {
+                case 'View all products.':
+                    viewProducts()
+                      .then(r =>{
+                          console.log(r)
+                      })
+                      .catch( e => console.log(e))
+                    break
+                case 'Search product and price.':
+                    // searchProductnPrice()
+                    break
+                case 'Make purchase.':
+                    // makePurchase()
+                    break
+                case '--EXIT--':
+                    process.exit()
+                    break
+                default:
+                getStart()
+                break
+            }
+        })
+        .catch(e => console.log(e))
+}
 // search a product by ID
 const searchProductnPrice = _ =>{
     prompt([{
@@ -28,22 +75,15 @@ const searchProductnPrice = _ =>{
         var qualityRequest = quality;
         yourOrder(itemid, quality)
     })
-    
+
 }
 
-const yourOrder = _ =>{
-    db.query('SELECT * FROM products', (e ,r )=>{
-        if (e) throw e
-        console.log(r)
-    })
-}
-// how many units to purchase
 // check the storage of the products
-// update remaining quality
+// update remaining quality  
 // calculate for total price
 
 
-db.connect(e => e ? console.log(e): yourOrder())
+db.connect(e => e ? console.log(e) : getStart())
 
 // check mysql connection
 // db.connect(e => e? console.log(e):console.log('successful connection'))
